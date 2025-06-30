ab3304d0fb939685043318662359e5462ea44e01
David-Brnb <321171197@ciencias.unam.mx>
2025-06-30 10:41:32 -0600

    Initial Commit

---
+++ MoonshotAppTests/MoonshotAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotAppTests.swift
+//  MoonshotAppTests
+//
+//  Created by Leoni Bernabe on 30/06/25.
+//
+
+import Testing
+@testable import MoonshotApp
+
+struct MoonshotAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
