e22657a957255f180e8fc5df754d6a22f57643b1
David-Brnb <321171197@ciencias.unam.mx>
2025-07-09 11:51:47 -0600

    now you can toogle between list and grid

---
+++ MoonshotApp/AstronautView.swift
@@ -0,0 +1,18 @@
+//
+//  AstronautView.swift
+//  MoonshotApp
+//
+//  Created by Leoni Bernabe on 03/07/25.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AstronautView()
+}
---
+++ MoonshotApp/components/CustomDivider.swift
@@ -0,0 +1,18 @@
+//
+//  CustomDivider.swift
+//  MoonshotApp
+//
+//  Created by Leoni Bernabe on 09/07/25.
+//
+
+import SwiftUI
+
+struct CustomDivider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomDivider()
+}
