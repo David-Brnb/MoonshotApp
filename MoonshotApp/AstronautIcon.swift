aab3088860a1359830eb81bf0203d00dd29a2400
David-Brnb <321171197@ciencias.unam.mx>
2025-07-09 11:57:18 -0600

    add: encapsulated some code

---
+++ MoonshotApp/AstronautIcon.swift
@@ -0,0 +1,18 @@
+//
+//  AstronautIcon.swift
+//  MoonshotApp
+//
+//  Created by Leoni Bernabe on 09/07/25.
+//
+
+import SwiftUI
+
+struct AstronautIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AstronautIcon()
+}
